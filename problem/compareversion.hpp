552568645d118eae62ef89d60984807b19471b1b
Lyndon Yang <cloudyfsail@outlook.com>
2015-01-27 08:22:43 +0000

    add CompareVersion

---
+++ problem/compareversion.hpp
@@ -0,0 +1,13 @@
+#ifndef _COMPARE_VERSION_HPP_
+#define _COMPARE_VERSION_HPP_
+namespace LeetCode ｛
+
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        
+    }
+};
+
+｝
+#endif
