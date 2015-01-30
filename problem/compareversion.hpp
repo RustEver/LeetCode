6da96fb42394ce17a5f2e5b4d95a9f793f4c9d29
Lyndon Yang <cloudyfsail@outlook.com>
2015-01-30 11:54:19 +0000

    solution for Compare Version Problem

--- problem/compareversion.hpp
+++ problem/compareversion.hpp
@@ -1,13 +1,33 @@
 #ifndef _COMPARE_VERSION_HPP_
 #define _COMPARE_VERSION_HPP_
-namespace LeetCode ｛
 
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+namespace LeetCode {
+    
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        
+        size_t i = 0;
+        size_t j = 0;
+        string str1, str2 = "0";
+        while(i < version1.size() || j < version2.size()) {
+            while (str1.back() != '.' && i < version1.size()) str1 += version1[i++];
+            while (str2.back() != '.' && j < version2.size()) str2 += version2[j++];
+            if (stoi(str1) > stoi(str2)) {
+                return 1;
+            } else if (stoi(str1) < stoi(str2)) {
+                return -1;
+            }
+            str1 = "0";
+            str2 = "0";
+        }
+        return 0;
     }
 };
 
-｝
+}
 #endif
